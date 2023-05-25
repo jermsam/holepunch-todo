b8e5de4e97ad26c3b517bdbca347cc3db17b6cb5
jermsam <ssali@jitpomi.com>
2023-05-24 19:36:18 -0600

    added  topic

--- src/p2pclient/index.mjs
+++ src/p2pclient/index.mjs
@@ -5,34 +5,32 @@ import Stream from '@hyperswarm/dht-relay/ws';
 import Hyperswarm from 'hyperswarm';
 import goodbye from 'graceful-goodbye';
 
-async function createTopic (topic) {
-  const prefix = 'some-app-prefix-'
-  const encoder = new TextEncoder();
-  const data = encoder.encode(prefix + topic);
- 
-  return await crypto.subtle.digest("SHA-256", data)
-  // return crypto.randomBytes(32)
-}
+// async function createTopic (topic) {
+//   const prefix = 'some-app-prefix-'
+//   const encoder = new TextEncoder();
+//   const data = encoder.encode(prefix + topic);
+//
+//   return await crypto.subtle.digest("SHA-256", data)
+//   // return crypto.randomBytes(32)
+// }
 
 export const socket = socketIO.connect("http://localhost:8080");
 
 const dht = new DHT(new Stream(true, socket))
 
-const swarm = new Hyperswarm({dht});
-
-const topic = await  createTopic('todo-list')
-
-if(topic) {
-  console.log('line 26: ',topic);
-  swarm.join(topic)
-}
+export const swarm = new Hyperswarm({dht});
 
 
-goodbye(async () => {
-  if(topic) {
-    await swarm.leave(topic)
-    await swarm.destroy()
-  }
-})
+  
+  const topicBuffer = Buffer.from('we-rock-todo', 'hex')
+  
+  console.log('line 26: ',topicBuffer);
+  swarm.join(topicBuffer)
+  
+  goodbye(async () => {
+   
+      await swarm.leave(topicBuffer)
+      await swarm.destroy()
+    
+  })
 
-export default swarm;
--- src/routes/todo/index.tsx
+++ src/routes/todo/index.tsx
@@ -1,5 +1,5 @@
 import {$, component$, useSignal, /*useStore,*/ useTask$} from '@builder.io/qwik';
-import { HiXCircleMini} from "@qwikest/icons/heroicons";
+import {HiXCircleMini} from '@qwikest/icons/heroicons';
 import {routeLoader$, z} from '@builder.io/qwik-city';
 import {formAction$, InitialValues, reset, SubmitHandler, useForm, zodForm$} from '@modular-forms/qwik';
 
@@ -8,7 +8,7 @@ import {formAction$, InitialValues, reset, SubmitHandler, useForm, zodForm$} fro
 
 
 import {isServer} from '@builder.io/qwik/build';
-import swarm, {socket} from '~/p2pclient/index.mjs';
+import {swarm, socket} from '~/p2pclient/index.mjs';
 // import {createHash} from 'crypto';
 // @ts-ignore
 // import crypto from 'hypercore-crypto'
@@ -17,7 +17,7 @@ import swarm, {socket} from '~/p2pclient/index.mjs';
 const todoSchema = z.object({
   text: z
     .string()
-    .min(1, 'Please enter item text.')
+    .min(1, 'Please enter item text.'),
 });
 
 type TodoForm = z.infer<typeof todoSchema>;
@@ -32,18 +32,22 @@ export const useFormAction = formAction$<TodoForm>((values) => {
 }, zodForm$(todoSchema));
 
 
+
+
 export default component$(() => {
-  const itemDialog = useSignal<HTMLDialogElement>()
-  const [todoForm, { Form, Field/*, FieldArray*/ }] = useForm<TodoForm>({
+  const itemDialog = useSignal<HTMLDialogElement>();
+  const [todoForm, {Form, Field/*, FieldArray*/}] = useForm<TodoForm>({
     loader: useFormLoader(),
     validate: zodForm$(todoSchema),
     action: useFormAction(),
   });
   
-  const todos = useSignal<TodoForm[]>([])
+
   
-  useTask$(()=>{
-    if(isServer) {
+  const todos = useSignal<TodoForm[]>([]);
+  
+  useTask$(() => {
+    if (isServer) {
       return;
     }
     
@@ -53,82 +57,79 @@ export default component$(() => {
         console.log({data, peerInfo});
         todos.value = data;
       });
-  
+      
       conn.on('close', () => {
         console.log('closed connection');
-      })
+      });
       
       conn.on('error', (e: any) => {
         console.log(e);
-      })
-     
-    })
-
-    // goodbye(async () => {
-    //   await swarm.leave(topic)
-    //   await swarm.destroy()
-    // })
-   
-  })
-  
-  const handleSubmit: SubmitHandler<TodoForm> = $((values, /*event*/) => {
-  
-      socket.onopen = (/*event*/) => {
-        socket.send(JSON.stringify(values));
-      };
+      });
+      
+    });
     
-    reset(todoForm)
-    itemDialog.value?.close()
+    
+  });
+  
+  const handleSubmit: SubmitHandler<TodoForm> = $((values /*event*/) => {
+    
+    socket.onopen = (/*event*/) => {
+      socket.send(JSON.stringify(values));
+    };
+    
+    reset(todoForm);
+    itemDialog.value?.close();
   });
   
   return (
     <section class="p-10">
       <button
         class="h-5 px-5 py-4  font-semibold rounded-md bg-black text-white flex items-center cursor-pointer shadow-2xl hover:shadow"
-        onclick$={()=>itemDialog.value?.showModal()}
+        onclick$={() => itemDialog.value?.showModal()}
       >
         Add Item
       </button>
-    <dialog ref={itemDialog} class={'rounded-2xl'}>
-      <div class={'bg-white p-10 min-w-[300px] relative'}>
-        <HiXCircleMini onClick$={()=>itemDialog.value?.close()} class={'absolute right-1 top-1 cursor-pointer'}/>
-        <div class={'flex flex-col gap-2'}>
-          <h3>Add Item</h3>
-          <Form
-            onSubmit$={$((values, event) => handleSubmit(values, event))}
-          >
-            <Field name="text">
-              {(field, props) => (
-                <div>
+     
+      <dialog ref={itemDialog} class={'rounded-2xl'}>
+        <div class={'bg-white p-10 min-w-[300px] relative'}>
+          <HiXCircleMini onClick$={() => itemDialog.value?.close()} class={'absolute right-1 top-1 cursor-pointer'}/>
+          <div class={'flex flex-col gap-2'}>
+            <h3>Add Item</h3>
+            <Form
+              onSubmit$={$((values, event) => handleSubmit(values, event))}
+            >
+              <Field name="text">
+                {(field, props) => (
+                  <div>
                   <textarea
-                  {...props}
-                  value={field.value}
-                  class="focus:ring-2 focus:ring-gray-950 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm"
-                
-                />
-                  {field.error && <div class={'text-red-800'}>{field.error}</div>}
-                </div>
-              )}
-            </Field>
-            <div class={'flex justify-end mt-5'}>
-              <button type="submit"
-                      class="h-5 px-5 py-4  font-semibold rounded-md bg-black text-white flex items-center cursor-pointer shadow-2xl hover:shadow">
-                Add
-              </button>
-            </div>
-          </Form>
-        </div>
-      </div>
-    </dialog>
-        <div>
-          {
-            todos.value.map((item, index) => (
-              <div key={index} class="w-96 bg-white shadow rounded">
-                {item.text}
+                    {...props}
+                    value={field.value}
+                    class="focus:ring-2 focus:ring-gray-950 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm"
+                  
+                  />
+                    {field.error && <div class={'text-red-800'}>{field.error}</div>}
+                  </div>
+                )}
+              </Field>
+              <div class={'flex justify-end mt-5'}>
+                <button type="submit"
+                        class="h-5 px-5 py-4  font-semibold rounded-md bg-black text-white flex items-center cursor-pointer shadow-2xl hover:shadow">
+                  Add
+                </button>
               </div>
-            ))
-          }
+            </Form>
+          </div>
         </div>
+      </dialog>
+      <div>
+        {
+          todos.value.map((item, index) => (
+            <div key={index} class="w-96 bg-white shadow rounded">
+              {item.text}
+            </div>
+          ))
+        }
+      </div>
     </section>
   );
 });
